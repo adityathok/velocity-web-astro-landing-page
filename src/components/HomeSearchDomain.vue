e9efb1a23f369062d4ec3b0655353717e10bb367
adityathok <aditya0thok@gmail.com>
2026-03-26 10:19:08 +0700

    feat(homepage): add client-side interactivity to domain search component
    Enable client-side hydration for HomeSearchDomain component to support Vue reactivity.
    Add click handlers to domain suggestion buttons that populate the search input.
    Bind search input to reactive data property for two-way data binding.

--- src/components/HomeSearchDomain.vue
+++ src/components/HomeSearchDomain.vue
@@ -7,15 +7,17 @@
                     Temukan Domain yang Sesuai<br>dengan Websitemu
                 </div>
                 <div class="space-x-1 mt-2">
-                    <span v-for="item in ['.com','.id','.co.id','.org']" class="inline-block text-xs bg-blue-600 hover:bg-blue-500 text-white px-3 py-1 rounded-xl cursor-pointer hover:shadow">
-                        {{ item }}
-                    </span>
+                    <template v-for="item in ['.com','.id','.co.id','.org']" >
+                        <span @click="changeKey(item)" class="inline-block text-xs bg-blue-600 hover:bg-blue-500 text-white px-3 py-1 rounded-xl cursor-pointer hover:shadow">
+                            {{ item }}
+                        </span>
+                    </template>
                 </div>
 
             </div>
             <div class="md:flex-1 mt-10 md:mt-0">
                 <form method="GET" action="/search-domain" class="flex bg-white border border-gray-200 px-3 py-2 w-full rounded-full">
-                    <input class="border-0! px-3 py-2 w-full rounded-full" name="key" placeholder="Cari domain ...">
+                    <input class="border-0! px-3 py-2 w-full rounded-full" name="key" v-model="keySearch" placeholder="Cari domain ...">
                     <Button type="submit" class="px-5! py-2!">
                         <Search />
                     </Button>
@@ -28,4 +30,11 @@
 <script setup>
 import { Search } from 'lucide-vue-next';
 import Button from '../components/Button.vue';
+import { ref } from 'vue';
+
+const keySearch = ref('')
+
+const changeKey = (val) => {
+    keySearch.value = val
+}
 </script>
